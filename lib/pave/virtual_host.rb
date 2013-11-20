0f229934aeb6d43470fb75a5ea36a23f1a01707c
Jamon Holmgren <jamon@clearsightstudio.com>
2013-11-20 08:04:33 -0800

    Added VirtualHost class

---
+++ lib/pave/virtual_host.rb
@@ -0,0 +1,24 @@
+module Pave
+  class VirtualHost
+
+  end
+end
+
+
+# /etc/apache2/extra/httpd-vhosts.conf
+# <Directory "/Users/jh/Code/Web/fishalaskamagazine.dev/">
+#   Allow From All
+#   AllowOverride All
+#   Options +Indexes
+# </Directory>
+# <VirtualHost *:80>
+#   ServerName "fishalaskamag.site"
+#   DocumentRoot "/Users/jh/Code/Web/fishalaskamagazine.dev"
+# </VirtualHost>
+
+# Update hosts file with IP
+# sudo apachectl restart
+
+
+
+  
