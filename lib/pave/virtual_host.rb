4305f8f55e7228c909fbebdf004d2ab09e6bcd5e
Ryan Linton <ryanlntn@gmail.com>
2013-11-21 08:41:32 -0800

    Remove dev comments

--- lib/pave/virtual_host.rb
+++ lib/pave/virtual_host.rb
@@ -149,22 +149,3 @@ module Pave
 
   end
 end
-
-
-# /etc/apache2/extra/httpd-vhosts.conf
-# <Directory "/Users/jh/Code/Web/fishalaskamagazine.dev/">
-#   Allow From All
-#   AllowOverride All
-#   Options +Indexes
-# </Directory>
-# <VirtualHost *:80>
-#   ServerName "fishalaskamag.site"
-#   DocumentRoot "/Users/jh/Code/Web/fishalaskamagazine.dev"
-# </VirtualHost>
-
-# Update hosts file with IP
-# sudo apachectl restart
-
-
-
-  
