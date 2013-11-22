5948956a4f661dec32d6888bb7ec8f1e9b171f7d
Ryan Linton <ryanlntn@gmail.com>
2013-11-22 13:22:47 -0800

    Start work on db:download and db:upload.

--- lib/pave/database.rb
+++ lib/pave/database.rb
@@ -18,12 +18,21 @@ module Pave
     end
 
     def setup
-      sh "mysql -u root -e 'CREATE DATABASE #{name}'"
+      sh "mysql -uroot -e 'CREATE DATABASE #{name}'"
     end
 
     def dump
       say "Creating dump of #{name} at #{Dir.getwd}/#{Time.now.strftime("%Y-%m-%d")}-#{name}.sql.gz"
-      sh "mysqldump -u root #{name} | gzip > #{Time.now.strftime("%Y-%m-%d")}-#{name}.sql.gz"
+      sh "mysqldump -uroot #{name} | gzip > #{Time.now.strftime("%Y-%m-%d")}-#{name}.sql.gz"
+    end
+
+    def download(host, user, password)
+      # Download the project's live database and replace local database.
+      # sh "mysqldump -h#{host} -u#{user} -p#{password} #{name} | mysql -uroot #{name}"
+    end
+
+    def upload
+      # Upload the project's local database and replace the live database.
     end
   end
 end
