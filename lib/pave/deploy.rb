b64cb3f65e46355956e4b2007d1fff20a4839cee
Ryan Linton <ryanlntn@gmail.com>
2013-12-05 21:30:19 -0800

    Add resources directory

--- lib/pave/deploy.rb
+++ lib/pave/deploy.rb
@@ -8,22 +8,10 @@ module Pave
       folder = ask "Enter the name of the remote directory (e.g. ~/webapps/appname/): "
       remote = ask "Enter the desired git remote name (e.g. production): "
 
-      # Create a temporary shell script to send to the remote server
-      sh "echo \"#!bin/bash\" > tempgitdeploysetup.sh"
-      sh "echo \"cd #{folder}\" >> tempgitdeploysetup.sh"
-      sh "echo \"if [ ! -d 'deploy.git' ]; then\" >> tempgitdeploysetup.sh"
-      sh "echo \"mkdir deploy.git && cd deploy.git\" >> tempgitdeploysetup.sh"
-      sh "echo \"git init --bare\" >> tempgitdeploysetup.sh"
-      sh "echo \"echo '#!/bin/sh' > hooks/post-receive\" >> tempgitdeploysetup.sh"
-      sh "echo \"echo 'GIT_WORK_TREE=.. git checkout -f' >> hooks/post-receive\" >> tempgitdeploysetup.sh"
-      sh "echo \"chmod +x hooks/post-receive\" >> tempgitdeploysetup.sh"
-      sh "echo \"fi\" >> tempgitdeploysetup.sh"
-
       puts "Piping shell script to #{server} for setup."
-      sh "ssh #{server} 'bash -s' < tempgitdeploysetup.sh"
+      script_path = File.join(File.expand_path("../..", File.dirname(__FILE__)), "resources/deploy.sh")
 
-      # Remove the temp file
-      sh "rm tempgitdeploysetup.sh"
+      sh "ssh #{server}:#{folder} 'bash -s' < #{script_path}"
 
       # Add git remote
       sh "git remote remove #{remote}"
