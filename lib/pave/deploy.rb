2b21e80ced4090370e561476c8d716ddb95422b3
Ryan Linton <ryanlntn@gmail.com>
2013-12-06 20:10:33 -0800

    Clean up deploy.rb a bit more

--- lib/pave/deploy.rb
+++ lib/pave/deploy.rb
@@ -3,22 +3,18 @@ module Pave
     include Pave::Shell
 
     def setup
-      # Get server, folder, and desired git remote name
-      server = ask "Enter the username and hostname of the remote server (e.g. user@server.com): "
-      folder = ask "Enter the name of the remote directory (e.g. ~/webapps/appname/): "
-      remote = ask "Enter the desired git remote name (e.g. production): "
+      server = ask "Username and hostname of the remote server (e.g. user@server.com): "
+      folder = ask "Name of the remote directory (e.g. ~/webapps/appname/): "
+      remote = ask "Desired git remote name (e.g. production): "
+      script = File.join(File.expand_path("../..", File.dirname(__FILE__)), "resources/deploy.sh")
 
       puts "Piping shell script to #{server} for setup."
-      script_path = File.join(File.expand_path("../..", File.dirname(__FILE__)), "resources/deploy.sh")
+      sh "ssh #{server}:#{folder} 'bash -s' < #{script}"
 
-      sh "ssh #{server}:#{folder} 'bash -s' < #{script_path}"
-
-      # Add git remote
       sh "git remote remove #{remote}"
       sh "git remote add #{remote} #{server}:#{folder}/deploy.git"
 
-      puts "Finished!"
-      puts "You may now run `pave deploy` to deploy your application."
+      puts "Finished! You may now run `pave deploy` to deploy your application."
     end
 
     def deploy(remote="production", branch="master")
