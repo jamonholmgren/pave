15c2ace1be915261dc6774d43fa5a17f2e87b096
Julian Glukhenko <yulian@clearsightstudio.com>
2013-12-12 11:57:46 -0800

    removed unnecessary code from script

--- templates/themes/blank/js/scripts.js
+++ templates/themes/blank/js/scripts.js
@@ -1,16 +1,7 @@
 $(document).ready(function(){
-  // on dom ready
-  winWidth = $(window).width();
-  winHeight = $(window).height();
-  winLocation = window.location;
 
-  console.log("Window Width: "+winWidth);
-  console.log("Window Height: "+winHeight);
-  console.log("Window Location: "+winLocation);
 
-});
 
-$(window).load(function(){
-// on window load
+
 
 });
