62d1feb1b685a60f8e7f41da02beadf2f017a9cd
Jamon Holmgren <jamon@clearsightstudio.com>
2014-02-13 12:36:15 -0800

    Added files:sync, made push and pull much faster with rsync.

--- lib/pave/files.rb
+++ lib/pave/files.rb
@@ -2,27 +2,30 @@ module Pave
   class Files
     include Pave::Shell
 
+    def self.exclusions
+      " --exclude 'files/tmp' --exclude 'files/cache' "
+    end
+
     def self.clear_cache
       sh "rm -rf ./files/tmp/*; rm -rf ./files/cache/*;"
     end
 
     def self.push(remote="live")
-      clear_cache
       server = Pave::Remote.server(remote)
       directory = Pave::Remote.directory(remote)
-      sh "scp -r ./files #{server}:#{directory}/local_files;"
-      sh "ssh #{server} 'cd #{directory};
-          mv ./files ./old_files;
-          mv ./local_files ./files && rm -rf ./old_files;'"
+      sh "rsync -u -r #{exclusions} ./files #{server}:#{directory}/files"
     end
 
     def self.pull(remote="live")
       server = Pave::Remote.server(remote)
       directory = Pave::Remote.directory(remote)
-      sh "scp -r #{server}:#{directory}/files ./remote_files;
-          mv ./files ./old_files;
-          mv ./remote_files ./files && rm -rf ./old_files;"
+      sh "rsync -u -r #{exclusions} #{server}:#{directory}/files ./files"
       clear_cache
+    end
+
+    def self.sync(remote="live")
+      pull(remote)
+      push(remote)
     end
   end
 end
