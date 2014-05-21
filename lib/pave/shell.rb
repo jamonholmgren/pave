1f72349c49c89cc285e2fc1c77f2bc2c62cbdd2f
Julian Glukhenko <yulian@clearsightstudio.com>
2014-05-21 12:45:43 -0700

    removed random puts

--- lib/pave/shell.rb
+++ lib/pave/shell.rb
@@ -17,10 +17,6 @@ module Pave
 
     def file_insert(insert_file, insert_pattern, insert_lines)
       file_string = File.read(insert_file).sub(insert_pattern, "#{insert_pattern}\n#{insert_lines}")
-      puts file_string
-      puts insert_pattern
-      puts insert_lines
-      puts insert_file
       File.write(insert_file, file_string)
     end
   end
